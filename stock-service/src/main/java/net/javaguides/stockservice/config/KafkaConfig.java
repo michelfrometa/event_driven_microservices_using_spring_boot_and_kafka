77432ba0d58ef81528ec19a6ec540be2133f8f15
Michel Lazaro Frometa Burey <michel@linkapital.com.br>
2024-04-29 03:40:53 -0300

    Refactoring, working as expected

--- stock-service/src/main/java/net/javaguides/stockservice/config/KafkaConfig.java
+++ stock-service/src/main/java/net/javaguides/stockservice/config/KafkaConfig.java
@@ -6,7 +6,6 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -16,7 +15,6 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 import java.util.HashMap;
 import java.util.Map;
 
-@EnableKafka
 @Configuration
 class KafkaConfig {
 
@@ -30,10 +28,12 @@ class KafkaConfig {
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, String.format("order:%s", OrderEvent.class.getName()));
+        props.put(JsonDeserializer.TYPE_MAPPINGS, "order:net.javaguides.stockservice.kafka.events.OrderEvent");
         return new DefaultKafkaConsumerFactory<>(
                 props,
                 new StringDeserializer(),
-                new JsonDeserializer<>(OrderEvent.class)
+                new JsonDeserializer<>()
         );
     }
 
