b3074a6caffbb0eb337cf5c96bce0dd13047edf8
Michel Lazaro Frometa Burey <michel@linkapital.com.br>
2024-04-29 03:47:35 -0300

    Refactor

--- stock-service/src/main/java/net/javaguides/stockservice/config/KafkaErrorHandler.java
+++ stock-service/src/main/java/net/javaguides/stockservice/config/KafkaErrorHandler.java
@@ -1,15 +1,14 @@
 package net.javaguides.stockservice.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.errors.RecordDeserializationException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.kafka.listener.CommonErrorHandler;
 import org.springframework.kafka.listener.MessageListenerContainer;
 
+@Slf4j
 class KafkaErrorHandler implements CommonErrorHandler {
-    private static final Logger log = LoggerFactory.getLogger(KafkaErrorHandler.class);
 
     @Override
     public boolean handleOne(Exception exception, ConsumerRecord<?, ?> record, Consumer<?, ?> consumer, MessageListenerContainer container) {
